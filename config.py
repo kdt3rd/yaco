63aaa8b326a68ce8025fcae9cf9c1f9b272cab51
Kimball Thurston <Kimball.Thurston@relianceada.com>
2012-12-19 15:29:59 -0800

    add config options for linux

--- config.py
+++ config.py
@@ -8,6 +8,20 @@ BuildConfig( "release", "Release" )
 
 if sys.platform.startswith( 'linux' ):
     sys.stdout.write( "Configuring for linux...\n" )
+    generator.DefineGlobal( 'CC', FindExecutable( "gcc" ) )
+    generator.DefineGlobal( 'CXX', FindExecutable( "g++" ) )
+    generator.DefineGlobal( 'LD', FindExecutable( "g++" ) )
+    generator.DefineGlobal( 'CXXFLAGS', [ "-std=c++0x" ] )
+    generator.DefineGlobal( 'AR', FindExecutable( "ar" ) )
+    generator.DefineGlobal( 'WARN', [ "-Wall" ] )
+#    generator.DefineGlobal( 'LDFLAGS', [ "-lc++" ] )
+#    generator.DefineGlobal( 'CC', FindExecutable( "clang" ) )
+#    generator.DefineGlobal( 'CXX', FindExecutable( "clang++" ) )
+#    generator.DefineGlobal( 'LD', FindExecutable( "clang++" ) )
+#    generator.DefineGlobal( 'CXXFLAGS', [ "-std=c++11", "-stdlib=libc++" ] )
+#    generator.DefineGlobal( 'AR', FindExecutable( "ar" ) )
+#    generator.DefineGlobal( 'WARN', [ "-Weverything", "-Wno-c++98-compat" ] )
+#    generator.DefineGlobal( 'LDFLAGS', [ "-lc++" ] )
 
 if sys.platform.startswith( 'darwin' ):
     sys.stdout.write( "Configuring for OS/X...\n" )
